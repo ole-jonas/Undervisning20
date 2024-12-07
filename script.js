4721fe66c440f61eb6d556978a9d4b34e67503bd
ole-jonas <olejonaskoder@gmail.com>
2024-12-07 15:34:16 +0100

    Line through

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const todoList = document.querySelector("#todoList");
 
 // Registers when button is clicked and activates a function.
 addButton.addEventListener("click", function (){
-    console.log("Leg til knappen er trykket!");
+    console.log("Legg til knappen er trykket!");
 
     // Creating a variable storing the value of another variable
     // Taking the global variable inputField and using the method .value to fetch a value stored in a new variable.
@@ -17,22 +17,37 @@ addButton.addEventListener("click", function (){
     todoItem.textContent = inputFieldValue;
     todoList.appendChild(todoItem);
 
+    // Creating the "Finished" button
     const finishedButton = document.createElement("button");
     finishedButton.textContent = "Ferdig";
     todoItem.appendChild(finishedButton);
 
+    // Adding event listener for the "Finished" button
     finishedButton.addEventListener("click", function () {
-        console.log("Ferdi knappen er trykket!");
-        todoItem.style.textDecoration = "line-through";
+        console.log("Ferdig knappen er trykket!");
+        // Toggles the strikethrough effect on the todo item
+        if (todoItem.style.textDecoration === "line-through") {
+            todoItem.style.textDecoration = "none"; // Remove striketrough
+        } else {
+            todoItem.style.textDecoration = "line-through"; // Add strikethrough
+        }
     });
 
-    const deleteButton = document.createElement("button")
+    // Creating the "Delete" button
+    const deleteButton = document.createElement("button");
     deleteButton.textContent = "Slett";
     todoItem.appendChild(deleteButton);
 
+    // Adding event listener for the "Delete" button
     deleteButton.addEventListener("click", function () {
         console.log("Slett knappen er trykket!");
-        todoItem.remove();
+
+        // Only remove the item if it has a strikethrough (i.e., its finished)
+        if (todoItem.style.textDecoration === "line-through") {
+            todoItem.remove(); // Remove the todo item from the list
+}  else {
+    console.log("The item is not finished yet and cannot be deleted.");
+}  
     });
 });
- 
+  
